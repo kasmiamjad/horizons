e7dd24fdb80c1a2db315a267b218d2b7ce3b4d08
kasmiamjad <kasmi.amjad@gmail.com>
2023-11-08 00:09:23 +0530

    changes in submit

--- src/pages/api/submit.ts
+++ src/pages/api/submit.ts
@@ -1,1 +1,9 @@
 
+export default async function handler(req:any, res:any) {
+  const body =req.body;
+  //console.log('Hassan');
+
+  //console.log(body);
+
+  res.status(200).json({ user:'success' });
+}
